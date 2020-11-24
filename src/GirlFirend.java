7d60defe7670781b28cdd33dd7ae400618076b9b
wxsmr <2757634617@qq.com>
2020-11-24 23:12:09 +0800

    java密码学

---
+++ src/GirlFirend.java
@@ -0,0 +1,26 @@
+public class GirlFirend {
+    String name;
+    int  sge;
+
+
+    public void kiss(){
+
+    }
+    public static void main(String [] args){
+        //System.out.println();
+    GirlFirend girl =new GirlFirend();
+    System.out.println();
+    System.out.println();
+    }
+    //现有数组
+    //int[] arr ={9,8,10,23,89,76,45,7,23,41};
+
+    /**
+     * 写两个方法,分别实现冒泡排序和选择排序，array进行升序排序
+     *  ① 选择排序: 方法名是xuanze, 接收一个int数组类型, 返回一个int数组类型
+     *  ② 冒泡排序: 方法名是maopao, 接收一个int数组类型，返回一个int数组类型
+     *  ③ 在main函数中，调用两个函数
+     */
+
+
+}
